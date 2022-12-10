dcc596e642df947c6bddb8cb667cd5e407afc293
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2022-12-10 04:31:55 +0300

    crating contact footer and responsive design

--- businessAgency/script.js
+++ businessAgency/script.js
@@ -37,5 +37,19 @@ video.addEventListener("timeupdate", () => {
     }
 })
 
-
 // section-2 video
+
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 70,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    }
+  });
+  
