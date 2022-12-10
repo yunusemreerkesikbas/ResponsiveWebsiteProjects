04fa46adb0ed47cb54aa7e35e8b84593f8623d3f
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2022-12-10 03:33:11 +0300

    create about us section and video added

--- businessAgency/script.js
+++ businessAgency/script.js
@@ -7,3 +7,35 @@ menu.addEventListener("click", () => {
 })
 
 //navbar
+
+// section-2 video
+const video = document.querySelector(".video")
+const btn = document.querySelector(".buttons i")
+const bar = document.querySelector(".video-bar")
+const playPause = () => {
+    if (video.paused) {
+        video.play()
+        btn.className = "far fa-pause-circle"
+        video.style.opacity = ".7"
+    }else{
+        video.pause()
+        btn.className = "far fa-play-circle"
+        video.style.opacity = ".3"
+
+    }
+    
+}
+btn.addEventListener("click", () => {
+    playPause();
+})
+video.addEventListener("timeupdate", () => {
+    // console.log(video.currentTime, video.duration)
+    const barWidth = video.currentTime / video.duration
+    bar.style.width = `${barWidth * 100}% `
+    if (video.ended) {
+        btn.className = "far fa-play-circle"
+    }
+})
+
+
+// section-2 video
